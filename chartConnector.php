34a71d2d587e5b0380a9b482aafa5f948c699e94
Ryan Hamilton <ryan.k.hamilton@wmich.edu>
2018-04-05 14:03:35 -0400

    Added PHP

--- chartConnector.php
+++ chartConnector.php
@@ -1,4 +1,24 @@
 <?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "espressopi";
 
+//Create Connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+//Check Connection
+if ($conn->connect_error) 
+{
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+//SQL Results
+//Temperature and Date Time
+$tempDateQuery = "SELECT * FROM temperature LIMIT 100";
+$tempDateResult = $conn->query($tempQuery);
+//Pressure
+//$pressureQuery = "";
+//$pressureResuly = $conn->query($pressureQuery);
 
 ?>
