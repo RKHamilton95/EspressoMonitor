ae13646a03401059e1c823c3e90d3bfd1bcf2ca5
Peter Shutt <peter.a.shutt@wmich.edu>
2018-04-14 12:45:54 -0400

    on

--- app.js
+++ app.js
@@ -31,6 +31,9 @@ connection.query('SELECT * FROM normal_data', function (error, rows){
 });
 })
 
+app.get('/on', function (req, res) {
+  res.send('POST request to the homepage')
+})
 
 
 app.use(express.static(__dirname + '/'));  
@@ -44,6 +47,29 @@ app.get('/', function(req, res,next) {
 server.listen(8056, () => console.log("Listening on port 8056"))
 
 
+
+
+
+app.get('/data', function (req, res) {
+  db.all("select * from irtemp where Timestamp >= Datetime('now', '-60 minutes')", function(err,rows){
+    res.json(rows);
+  });
+})
+
+
+ 
+app.get('/', function (req, res) {
+  res.sendFile((__dirname + '/client.html'));
+})
+ 
+app.listen(3000)
+
+
+process.on('SIGINT', function() {
+    console.log("Caught interrupt signal");
+    db.close();
+    process.exit();
+});
 
 
 
